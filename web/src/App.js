3abfc503a7d93a2aaf2959443b56e8f3f12246bc
Thai Le <thai.le@calian.com>
2022-03-08 20:01:40 -0700

    complete components

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import { HashRouter, Switch, Redirect, Route } from "react-router-dom";
+
+import HomePage from "./components/HomePage/HomePage"
+import SignupPage from "./components/SignupPage/SignupPage"
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={SignupPage} />
+        <Route exact path="/home" component={HomePage} />
+      </Switch>
+    </HashRouter>
+  );
+}
---
+++ web/src/backend.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+// import Auth from '../stores/auth';
+
+const API = 'http://localhost:4000';
+
+// function authHeader() {
+//   return { headers: { Authorization: `Bearer ${Auth.getToken()}` } };
+// }
+
+function ApiException(message, status) {
+  this.message = message;
+  this.status = status;
+}
+
+function axiosError(e, defaultMessage) {
+  console.warn(e);
+  if (e.response && e.response.data && e.response.data.message) {
+    throw new ApiException(e.response.data.message, e.response.status);
+  }
+  throw new ApiException(defaultMessage ? defaultMessage : `Invalid response from server`);
+}
+
+export default {
+  register: async(email) => {
+    try {
+      await axios.post(`${API}/`, { email });
+    } catch (e) {
+      axiosError(e);
+    }
+  }
+};
