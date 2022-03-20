8cd58fbb8a2fc11955964abad107c65de6e7988c
Thai Le <thai.le@calian.com>
2022-03-19 20:53:45 -0600

    fix indentation

--- admin/app.js
+++ admin/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const jsonfile = require('jsonfile')
 const { Wallets } = require('fabric-network');
 const { connectionProfileOrg1, caClient } = require('../utils');
 const userWalletCreated = user => fs.existsSync(`./wallet/${user}.id`);
@@ -17,61 +18,61 @@ let peer;
 let ca;
 
 async function init() {
-	// Note: wallet can be built in memory as well
-	wallet = await Wallets.newFileSystemWallet(WALLET_PATH);
-	peer = connectionProfileOrg1();
-	ca = caClient(peer, CA_HOST);
+  // Note: wallet can be built in memory as well
+  wallet = await Wallets.newFileSystemWallet(WALLET_PATH);
+  peer = connectionProfileOrg1();
+  ca = caClient(peer, CA_HOST);
 }
 
 async function admin() {
-	if (userWalletCreated(ADMIN_ID) || (await wallet.get(ADMIN_ID))) { return; }
-	const enrollment = await ca.enroll({
-		enrollmentID: ADMIN_ID,
-		enrollmentSecret: ADMIN_PWD
-	});
-	await wallet.put(ADMIN_ID, {
-		credentials: {
-			certificate: enrollment.certificate,
-			privateKey: enrollment.key.toBytes(),
-		},
-		mspId: MSP,
-		type: 'X.509',
-	});
+  if (userWalletCreated(ADMIN_ID) || (await wallet.get(ADMIN_ID))) { return; }
+  const enrollment = await ca.enroll({
+    enrollmentID: ADMIN_ID,
+    enrollmentSecret: ADMIN_PWD
+  });
+  await wallet.put(ADMIN_ID, {
+    credentials: {
+      certificate: enrollment.certificate,
+      privateKey: enrollment.key.toBytes(),
+    },
+    mspId: MSP,
+    type: 'X.509',
+  });
 }
 
 async function registerEmail(email) {
-	debugger;
-	const adminIdentity = await wallet.get(ADMIN_ID);
-	if ((await wallet.get(email)) || !adminIdentity) return;
-	const adminUser = await wallet
-		.getProviderRegistry()
-		.getProvider(adminIdentity.type)
-		.getUserContext(adminIdentity, ADMIN_ID);
-	return await ca.register({
-		affiliation: AFFILIATION,
-		enrollmentID: email,
-		role: 'client'
-	}, adminUser);
+  debugger;
+  const adminIdentity = await wallet.get(ADMIN_ID);
+  if ((await wallet.get(email)) || !adminIdentity) return;
+  const adminUser = await wallet
+    .getProviderRegistry()
+    .getProvider(adminIdentity.type)
+    .getUserContext(adminIdentity, ADMIN_ID);
+  return await ca.register({
+    affiliation: AFFILIATION,
+    enrollmentID: email,
+    role: 'client'
+  }, adminUser);
 }
 
 async function emails() {
-	process.argv.shift();
-	process.argv.shift();
-	const secrets = [];
-	for (const email of process.argv) {
-		secrets.push(await registerEmail(email));
-	}
-	console.log(secrets);
+  process.argv.shift();
+  process.argv.shift();
+  const secrets = {};
+  for (const email of process.argv) {
+    secrets[email] = await registerEmail(email);
+  }
+  jsonfile.writeFile('secrets.json', secrets);
 }
 
 async function main() {
-	try {
-		await init();
-		await admin();
-		await emails();
-	} catch (e) {
-		console.error(e);
-	}
+  try {
+    await init();
+    await admin();
+    await emails();
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 main();
--- backend/index.js
+++ backend/index.js
@@ -1,63 +1,15 @@
 const express = require('express');
+const user = require('user');
 
 const app = express();
 
-app.get('/', function(req, res){
-  res.send("Hello world!");
+app.post('/enroll', async (req, res) => {
+  try {
+    await user.enroll(req.body.email, req.body.secret);
+  } catch (err) {
+    // next(err);
+    res.error(500);
+  }
 });
 
 app.listen(4000);
-
-'use strict';
-
-const { Gateway, Wallets } = require('fabric-network');
-const FabricCAServices = require('fabric-ca-client');
-const path = require('path');
-const { buildCAClient, registerAndEnrollUser, enrollAdmin } = require('./CAUtil.js');
-const { buildCCPOrg1, buildWallet, adminAndUserCreated } = require('./AppUtil.js');
-
-const channelName = 'mychannel';
-const chaincodeName = 'chaincode1';
-const mspOrg1 = 'Org1MSP';
-const walletPath = path.join(__dirname, 'wallet');
-const org1UserId = 'appUser';
-
-const prettyJSONString = (inputString) => JSON.stringify(JSON.parse(inputString), null, 2);
-const userWalletCreated = () => fs.existsSync('./wallet/user.id');
-
-// Enroll with the CA first
-// then register with the contract 
-async function register(caClient, wallet) {
-  await enrollAdmin(caClient, wallet, mspOrg1);
-  await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department1');
-}
-
-async function main() {
-  try {
-    const peer = buildCCPOrg1();
-    const caClient = buildCAClient(FabricCAServices, peer, 'ca.org1.example.com');
-    const wallet = await buildWallet(Wallets, walletPath);
-
-    if (!userWalletCreated()) {
-      await adminAndUser(caClient, wallet);
-    }
-
-    const gateway = new Gateway();
-
-    try {
-      await gateway.connect(peer, {
-        wallet,
-        identity: org1UserId,
-        discovery: { enabled: true, asLocalhost: true }
-      });
-
-      const network = await gateway.getNetwork(channelName);
-      const contract = network.getContract(chaincodeName);
-      await contract.submitTransaction('InitLedger');
-    } finally {
-      gateway.disconnect();
-    }
-  } catch (error) {
-    console.error(`******** FAILED to run the application: ${error}`);
-  }
-}
