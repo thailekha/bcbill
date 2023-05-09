9a7b6ef35c52da8fac57459b78698ec45bb4071a
thailekha <thailekha@yahoo.com>
2023-05-09 02:36:03 +0000

    compress wallet

--- backend/services/crypt.js
+++ backend/services/crypt.js
@@ -1,12 +1,14 @@
 const crypto = require('crypto');
+const zlib = require('zlib');
 
 const secret = 'be864174656596ae7fd818e32236dfddf96f2e335fc18600f0290906a051ebf2';
 const iv = '0505c698405c5a5b34c609c3c7e2fd33';
 
 function encrypt(message) {
+  const compressed = zlib.deflateSync(message).toString('binary');
   const cipher = crypto.createCipheriv('aes-256-cbc',
     Buffer.from(secret, 'hex'), Buffer.from(iv, 'hex'));
-  let encrypted = cipher.update(message, 'utf-8', 'hex');
+  let encrypted = cipher.update(compressed, 'binary', 'hex');
   encrypted += cipher.final('hex');
   return encrypted;
 }
@@ -14,9 +16,9 @@ function encrypt(message) {
 function decrypt(encryptedData) {
   const decipher = crypto.createDecipheriv('aes-256-cbc',
     Buffer.from(secret, 'hex'), Buffer.from(iv, 'hex'));
-  let decrypted = decipher.update(encryptedData, 'hex', 'utf-8');
-  decrypted += decipher.final('utf-8');
-  return decrypted;
+  const decrypted = Buffer.concat([decipher.update(encryptedData, 'hex'), decipher.final()]);
+  const decompressed = zlib.inflateSync(decrypted);
+  return decompressed.toString();
 }
 
 module.exports = {
